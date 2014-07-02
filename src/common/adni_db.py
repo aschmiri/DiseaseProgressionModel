d43005c865e47702fc2711f67a531549def810b9
aschmiri <a.schmidtrichberg@gmail.com>
2014-07-02 17:44:12 +0100

    Added adnimerge_cog and diagdates tables to adni.db

--- src/common/adni_db.py
+++ src/common/adni_db.py
@@ -8,10 +8,66 @@ import sqlite3
 
 ################################################################################
 #
-# create_adni_db
+# crate_adnimerge_db
+#
+################################################################################   
+def crate_adnimerge_db(): 
+    import rpy2.robjects as robjects
+    
+    # Lead adnimerge
+    robjects.r['load']( os.path.join( adni.merge_folder, 'data/adnimerge.rdata' ) )
+    adnimerge = robjects.r['adnimerge']
+    
+    # Open database conenction
+    con = sqlite3.connect( os.path.join( adni.project_folder, 'lists', 'adni.db' ) )
+    cur = con.cursor()
+
+    # Create table
+    cur.execute( 'DROP TABLE IF EXISTS adnimerge_cog' )
+    cur.execute( 'CREATE TABLE adnimerge_cog (\
+                                          rid INTEGER, \
+                                          viscode TEXT, \
+                                          apoe4 INTEGER, \
+                                          cdrsb REAL, \
+                                          adas11 REAL, \
+                                          adas13 REAL, \
+                                          faq INTEGER, \
+                                          moca INTEGER, \
+                                          PRIMARY KEY(rid, viscode))' )
+
+    # Print each row
+    for i in range(adnimerge.nrow):
+            rid      = adnimerge[adnimerge.colnames.index('RID')][i]
+            viscode  = adnimerge[adnimerge.colnames.index('VISCODE')][i]
+            apoe4    = adnimerge[adnimerge.colnames.index('APOE4')][i]
+            cdrsb    = adnimerge[adnimerge.colnames.index('CDRSB')][i]
+            adas11   = adnimerge[adnimerge.colnames.index('ADAS11')][i]
+            adas13   = adnimerge[adnimerge.colnames.index('ADAS13')][i]
+            faq      = adnimerge[adnimerge.colnames.index('FAQ')][i]
+            moca     = adnimerge[adnimerge.colnames.index('MOCA')][i]
+            try:
+                cur.execute( "INSERT INTO adnimerge_cog VALUES (?,?,?,?,?,?,?,?)",
+                             (rid, viscode, apoe4, cdrsb, adas11, adas13, faq, moca) )
+            except:
+                print 'WARNING: ignoring double entry for', rid, viscode
+    
+    cur.execute( "UPDATE adnimerge_cog SET apoe4=NULL WHERE apoe4='NA'" )
+    cur.execute( "UPDATE adnimerge_cog SET cdrsb=NULL WHERE cdrsb='NA'" )
+    cur.execute( "UPDATE adnimerge_cog SET adas11=NULL WHERE adas11='NA'" )
+    cur.execute( "UPDATE adnimerge_cog SET adas13=NULL WHERE adas13='NA'" )
+    cur.execute( "UPDATE adnimerge_cog SET faq=NULL WHERE faq='NA'" )
+    cur.execute( "UPDATE adnimerge_cog SET moca=NULL WHERE moca='NA'" )
+      
+    # Commit and close database connection
+    con.commit()
+    con.close()     
+
+################################################################################
+#
+# create_adniquery_db
 #
 ################################################################################
-def create_adni_db():
+def create_adniquery_db():
     # Open database conenction
     con = sqlite3.connect( os.path.join( adni.project_folder, 'lists', 'adni.db' ) )
     cur = con.cursor()
@@ -29,13 +85,7 @@ def create_adni_db():
                                           scandate TEXT, \
                                           fieldstrength TEXT, \
                                           age REAL, \
-                                          apoe4 INTEGER, \
-                                          cdrsb REAL, \
-                                          adas11 REAL, \
-                                          adas13 REAL, \
-                                          faq INTEGER, \
-                                          mmse INTEGER, \
-                                          moca INTEGER)' )
+                                          mmse INTEGER)' )
     
     # Read database from query file 
     query_file = os.path.join( adni.project_folder, 'lists', 'query_ADNI.csv' )
@@ -44,7 +94,7 @@ def create_adni_db():
         for entry in entries:
             iid      = entry['ImageUID']
             rid      = entry['RID']
-            viscode      = entry['VISCODE']
+            viscode  = entry['VISCODE']
             study_bl = entry['ORIGPROT']
             study    = entry['COLPROT']
             filename = os.path.basename( entry['Files'] )
@@ -53,17 +103,11 @@ def create_adni_db():
             scandate = entry['ScanDate']
             fs       = entry['MagStrength']
             age      = entry['AGE.scan']
-            apoe4    = 0#entry['APOE4']
-            cdrsb    = 0#entry['CDRSB']
-            adas11   = 0#entry['ADAS11']
-            adas13   = 0#entry['ADAS13']
-            faq      = 0#entry['FAQ']
             mmse     = entry['MMSE']
-            moca     = 0#entry['MOCA']
+            
             if (study == 'ADNI1' and float(fs) < 2.0) or (study != 'ADNI1' and float(fs) > 2.0):
-                cur.execute( "INSERT INTO adnimerge VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-                             (iid, rid, viscode, study_bl, study, filename, dx_bl, dx, scandate, fs, age, 
-                              apoe4, cdrsb, adas11, adas13, faq, mmse, moca) )
+                cur.execute( "INSERT INTO adnimerge VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
+                             (iid, rid, viscode, study_bl, study, filename, dx_bl, dx, scandate, fs, age, mmse) )
             
     # Commit and close database connection
     con.commit()
@@ -72,21 +116,23 @@ def create_adni_db():
 
 ################################################################################
 #
-# get_years_after_symptoms
+# crate_years_after_diag_db
 #
 ################################################################################
-def get_years_after_symptoms():
-    con = sqlite3.connect(":memory:")
+def crate_years_after_diag_db():
+    # Open database conenction
+    con = sqlite3.connect( os.path.join( adni.project_folder, 'lists', 'adni.db' ) )
     cur = con.cursor()
     
     #
     # Read dates database 
     #
-    cur.execute( "CREATE TABLE dates (rid INT PRIMARY KEY, \
-                                      viscode TEXT, \
-                                      year_symptom INT, \
-                                      year_mci INT, \
-                                      year_ad INT)" )
+    cur.execute( 'DROP TABLE IF EXISTS diagdates' )
+    cur.execute( "CREATE TABLE diagdates (rid INT PRIMARY KEY, \
+                                          viscode TEXT, \
+                                          year_symptom INT, \
+                                          year_mci INT, \
+                                          year_ad INT)" )
 
     file_dates = os.path.join( adni.project_folder, 'lists/ADNI_diag_dates.csv' )
     
@@ -99,107 +145,16 @@ def get_years_after_symptoms():
             year_mci     = entry['Year MCI diagnosed']
             year_ad      = entry['Year AD diagnosed']
             if viscode == 'sc' or viscode == 'v01':
-                cur.execute( "INSERT INTO dates VALUES (?,?,?,?,?)", 
+                cur.execute( "INSERT INTO diagdates VALUES (?,?,?,?,?)", 
                              (rid, viscode, year_symptom, year_mci, year_ad) )
                 
-    cur.execute( "UPDATE dates SET year_symptom=NULL WHERE year_symptom=''" )
-    cur.execute( "UPDATE dates SET year_mci=NULL WHERE year_mci=''" )
-    cur.execute( "UPDATE dates SET year_ad=NULL WHERE year_ad=''" )
-        
-    #
-    # Read dpi database 
-    #
-    cur.execute( "CREATE TABLE dpis (rid INT PRIMARY KEY, \
-                                     dpi FLOAT)")
-
-    file_dates = os.path.join( adni.project_folder, 'atlas/model_1/data_sym_m24_AD.csv' )
-    
-    with open( file_dates, 'rb' ) as csvfile:
-        entries = csv.DictReader( csvfile )
-        for entry in entries:
-            rid      = entry['RID']
-            dpi      = entry['DPI']
-            cur.execute( "INSERT INTO dpis VALUES (?,?)", 
-                         (rid, dpi) )
-    
-    #
-    # Read summary database 
-    #
-    cur.execute( "CREATE TABLE summary (rid INT PRIMARY KEY, \
-                                        filename TEXT, \
-                                        diagnosis TEXT, \
-                                        scandate TEXT, \
-                                        age FLOAT, \
-                                        apoe4 INT, \
-                                        cdrsb FLOAT, \
-                                        adas11 FLOAT, \
-                                        adas13 FLOAT, \
-                                        faq INT, \
-                                        mmse INT, \
-                                        moca INT)" )
-
-    def add_summary( folder, filename ):
-        with open( filename, 'rb' ) as csvfile:
-            entries = csv.DictReader( csvfile )
-            for entry in entries:
-                filename = os.path.join( folder, entry['Files'] )
-                if not os.path.isfile( filename ):
-                    filename = filename.replace( '.nii.gz', '.dof.gz' )
-                if os.path.isfile( filename ):
-                    rid      = entry['RID']
-                    dx       = entry['DX.bl']
-                    scandate = entry['ScanDate']
-                    age      = entry['AGE']
-                    apoe4    = entry['APOE4']
-                    cdrsb    = entry['CDRSB']
-                    adas11   = entry['ADAS11']
-                    adas13   = entry['ADAS13']
-                    faq      = entry['FAQ']
-                    mmse     = entry['MMSE']
-                    moca     = entry['MOCA']
-                    cur.execute( "INSERT INTO summary VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
-                                 (rid, filename, dx, scandate, age, apoe4, cdrsb, adas11, adas13, faq, mmse, moca) )
-
-    add_summary( '/vol/medic01/users/aschmidt/projects/Data/ADNI/data/ADNI1/native/images', 
-                 os.path.join( adni.project_folder, 'lists/query_ADNI1.csv' ) )
-    add_summary( '/vol/medic01/users/aschmidt/projects/Data/ADNI/data/ADNI2/native/images', 
-                 os.path.join( adni.project_folder, 'lists/query_ADNI2.csv' ) )
-    add_summary( '/vol/medic01/users/aschmidt/projects/Data/ADNI/data/ADNIGO/native/images', 
-                 os.path.join( adni.project_folder, 'lists/query_ADNIGO.csv' ) )
-    
-    cur.execute( "UPDATE summary SET apoe4=NULL WHERE apoe4='NA'" )
-    cur.execute( "UPDATE summary SET cdrsb=NULL WHERE cdrsb='NA'" )
-    cur.execute( "UPDATE summary SET adas11=NULL WHERE adas11='NA'" )
-    cur.execute( "UPDATE summary SET adas13=NULL WHERE adas13='NA'" )
-    cur.execute( "UPDATE summary SET faq=NULL WHERE faq='NA'" )
-    cur.execute( "UPDATE summary SET mmse=NULL WHERE mmse='NA'" )
-    cur.execute( "UPDATE summary SET moca=NULL WHERE moca='NA'" )
-    
-    cur.execute( "SELECT rid, scandate, year_symptom, dpi, cdrsb, adas11, adas13, faq, mmse, moca \
-                  FROM summary NATURAL JOIN dates NATURAL JOIN dpis" )
-    
-    rows = cur.fetchall()
-    years  = []
-    dpi    = []
-    cdrsb  = []
-    adas11 = []
-    adas13 = []
-    faq    = []
-    mmse   = []
-    moca   = [] 
-    for row in rows:
-        if row[2] != None:
-            print row
-            scan_year  = int(row[1][0:4])
-            years.append( scan_year - row[2] )
-            dpi.append( row[3] )
-            cdrsb.append( row[4] )
-            adas11.append( row[5] )
-            adas13.append( row[6] )
-            faq.append( row[7] )
-            mmse.append( row[8] )
-            moca.append( row[9] )
-    return years, dpi, cdrsb, adas11, adas13, faq, mmse, moca
+    cur.execute( "UPDATE diagdates SET year_symptom=NULL WHERE year_symptom=''" )
+    cur.execute( "UPDATE diagdates SET year_mci=NULL WHERE year_mci=''" )
+    cur.execute( "UPDATE diagdates SET year_ad=NULL WHERE year_ad=''" )
+             
+    # Commit and close database connection
+    con.commit()
+    con.close()
 
 ################################################################################
 #
@@ -226,20 +181,22 @@ def print_data_for_rid( rid = 43 ):
                 print adnimerge[col][row],
             print ''
 
+
 ################################################################################
 #
 # main
 #
 ################################################################################
 if __name__ == "__main__":
-    #get_years_after_symptoms()
-    print_data_for_rid( 54 )
-    #create_adni_db()
+    #crate_years_after_diag_db()
+    #create_adniquery_db()
+    #crate_adnimerge_db()
+    #print_data_for_rid( 101 )
     
     # Test DB
     con = sqlite3.connect( os.path.join( adni.project_folder, 'lists', 'adni.db' ) )
     cur = con.cursor()
-    cur.execute( "SELECT iid, rid, viscode, study, study_bl, fieldstrength, mmse FROM adnimerge WHERE rid = 54" )
+    cur.execute( "SELECT iid, rid, viscode, study, study_bl, fieldstrength, mmse, faq, moca, cdrsb FROM adnimerge JOIN adnimerge_cog USING (rid, viscode) WHERE rid = 54" )
     
     rows = cur.fetchall()
     for row in rows:
