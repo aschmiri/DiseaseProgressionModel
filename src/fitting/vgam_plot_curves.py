883cd20fff42f96c07690d35f31b80721f2d5b67
aschmiri <a.schmidtrichberg@gmail.com>
2014-07-18 14:15:16 +0100

    Minor viual enhancements in plotting

--- src/fitting/vgam_plot_curves.py
+++ src/fitting/vgam_plot_curves.py
@@ -24,65 +24,84 @@ def read_dens_file( dens_file ):
     return np.asarray( y ), np.asarray( densities )
         
     
-def plot( score_name, points_file, curves_file, plot_points, save_file=False, dens_file=None, min_progression=-42 ):
-    # Load csv in a structured array
-    r = mlab.csv2rec( curves_file )
-    plt.figure( figsize=(13, 7), dpi=100 )
+def plot( score_name, points_file, curves_file, plot_points, save_file=False, dens_file=None ):
+    plt.figure( figsize=(12, 5), dpi=100 )
     
-    # Sort the array on age
+    #
+    # Get data
+    #
+    r = mlab.csv2rec( curves_file )
     r_sorted = np.sort( r, order=r.dtype.names[2] )
     progrs   = r_sorted[r.dtype.names[2]]
     curves   = [r_sorted['x1'],  r_sorted['x5'],  r_sorted['x25'], r_sorted['x50'],
                 r_sorted['x75'], r_sorted['x95'], r_sorted['x99']]
-    labels   = ['1%', '5%', '25%', '50%', '75%', '95%', '99%']
-    greyvals = ['0.6', '0.4', '0.2', '0', '0.2', '0.4', '0.6']
-    x_offset = 0.5
-    y_offset = 0.5
+    min_progress = np.min(  progrs )
+    max_progress = np.max(  progrs )
+    progress_offset = (max_progress - min_progress) * 0.1
     
+    #
+    # Plot densities
+    #
     if dens_file != None:
         y, densities = read_dens_file( dens_file )
         
-        ax1 = plt.subplot(1,2,1)
-        ax2 = plt.subplot(1,2,2)
+        ax1 = plt.subplot( 1, 2, 1 )
+        ax2 = plt.subplot( 1, 2, 2 )
         ax2.set_title( 'Probability density distributions for ' + score_name )
         ax2.set_xlabel( 'Metric value' )
         ax2.set_ylabel( 'Probability' )
+
             
-        min_val = np.min(curves)
-        max_val = np.max(curves)
+        min_val = np.min( curves )
+        max_val = np.max( curves )
         progr_samples = [-36, -18, 0, 18, 36 ]
-        print min_val,max_val
         
         sample_cmap = cmx.ScalarMappable(
-            norm = colors.Normalize( vmin=0, vmax=len(progr_samples)-1 ), 
-            cmap = plt.get_cmap('winter') )
+            norm = colors.Normalize( vmin=-len(progr_samples)+1, vmax=len(progr_samples)-1 ), 
+            cmap = plt.get_cmap( adni.adni_cmap ))
         
         for progr in progr_samples:
             sample_color = sample_cmap.to_rgba( progr_samples.index(progr) )
-            ax1.axvline( progr, color=sample_color, linestyle='--', alpha=0.5 )
+            ax1.axvline( progr, color=sample_color, linestyle='--', alpha=0.8 )
             ax2.set_xlim( min_val, max_val )
-            ax2.plot( y, densities[progr + min_progression], label=str(progr), color=sample_color )
+            ax2.plot( y, densities[progr + min_progress], label=str(progr), color=sample_color )
+            
+        handles2, labels2 = ax2.get_legend_handles_labels()
+        ax2.legend( handles2, labels2, fontsize=10 )
     else:
-        ax1 = plt.subplot(111)
+        ax1 = plt.subplot( 1, 1, 1 )
     
+    #
+    # Plot points
+    #
     if plot_points:
-        # Collect all measurement points from the measurement file
         m = mlab.csv2rec( points_file )
         progr_points = m['progress']
         value_points = m['value']
-        ax1.scatter( progr_points, value_points, vmin=0.0, vmax=1.0, linewidths=0, cmap=adni.adni_cmap, alpha=0.25 )
+        diagn_points = [0.5 if p < 0 else 1.0 for p in progr_points]
+        ax1.scatter( progr_points, value_points, c=diagn_points, vmin=0.0, vmax=1.0, linewidths=0, cmap=adni.adni_cmap, alpha=0.25 )
     
+    #
     # Plot the percentile curves
+    #
+    labels   = ['1%', '5%', '25%', '50%', '75%', '95%', '99%']
+    greyvals = ['0.45', '0.3', '0.15', '0', '0.15', '0.3', '0.45']
+    #styles   = ['g-', 'g-', 'g-', 'b-', 'g-', 'g-', 'g-']
+
     for (curve, greyval, label) in zip( curves, greyvals, labels ):
         ax1.plot( progrs, curve, color=greyval )
-        ax1.text( progrs[-1]+x_offset, curve[-1]-y_offset, label )
+        ax1.text( progrs[-1]+5, curve[-1]-0.5, label, fontsize=10 )
     
     ax1.set_title( 'Percentile curves for ' + score_name )
     ax1.set_xlabel( 'Disease progression relative to point of conversion' )
     ax1.set_ylabel( 'Metric value' )
+    ax1.set_xlim( min_progress-progress_offset, max_progress+2*progress_offset )
+    
     plt.tight_layout()
 
-    # Draw the plot
+    #
+    # Draw or save the plot
+    #
     if save_file:
         plot_filename = curves_file.replace( '.csv', '_plot.png' )
         plt.savefig( plot_filename, dpi=100 )
