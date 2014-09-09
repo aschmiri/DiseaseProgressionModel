2289fe97220e0be58386aee73b7c41eacd325005
aschmiri <a.schmidtrichberg@gmail.com>
2014-09-09 14:26:21 +0100

    Added ability to plot and analyse synth model evaluation

--- src/fitting/vgam_evaluate_synth_model.py
+++ src/fitting/vgam_evaluate_synth_model.py
@@ -5,6 +5,7 @@ import csv
 import numpy as np
 from subprocess import call
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 from common import log as log
 from common import adni_tools as adni
 from vgam.datahandler import SynthDataHandler
@@ -14,20 +15,53 @@ from vgam.progressionmodel import ProgressionModel
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--number_of_runs', type=int, default=5, help='the number of repeated runs')
-    parser.add_argument('-p', '--number_of_progression_steps', type=int, default=10, help='the number of progression steps')
-    parser.add_argument('-v', '--number_of_value_steps', type=int, default=1000, help='the number of value steps')
-    parser.add_argument('-g', '--progression_range', type=int, default=2000, help='the width of progression range window')
+    parser.add_argument('-r', '--recompute_errors', action='store_true', help='recompute the errors of the models')
+    parser.add_argument('--range', type=int, nargs=3, default=[100, 2000, 100], help='the range for the number of samples tested')
+    parser.add_argument('--number_of_runs', type=int, default=5, help='the number of repeated runs')
+    parser.add_argument('--number_of_progression_steps', type=int, default=10, help='the number of progression steps')
+    parser.add_argument('--number_of_value_steps', type=int, default=1000, help='the number of value steps')
+    parser.add_argument('--progression_range', type=int, default=2000, help='the width of progression range window')
     args = parser.parse_args()
 
     data_handler = SynthDataHandler()
 
-    errors = {}
-    for num_samples in range(100, 2000, 100):
-        errors.update({num_samples: evaluate_sample_number(args, data_handler, num_samples)})
+    if args.recompute_errors:
+        errors = compute_errors(args, data_handler)
+        save_errors(args, data_handler, errors)
+    else:
+        errors = read_errors(args)
 
-    save_errors(args, data_handler, errors)
     plot_errors(args, data_handler, errors)
+    analyse_errors(args, data_handler, errors)
+
+
+def read_errors(args):
+    evaluation_file = os.path.join(adni.eval_folder, 'eval_synth_model.csv')
+
+    m = mlab.csv2rec(evaluation_file)
+    biomarkers = m['biomarker']
+    uniforms = m['uniform']
+    experiments = np.sort([int(e) for e in m.dtype.names[2:]])
+
+    errors = {}
+    for experiment in experiments:
+        errors.update({experiment: {}})
+        measures = m[str(experiment)]
+        for i in xrange(len(biomarkers)):
+            biomarker = biomarkers[i]
+            uniform = uniforms[i]
+            if biomarker not in errors[experiment]:
+                errors[experiment].update({biomarker: {}})
+            errors[experiment][biomarker].update({uniform: measures[i]})
+
+    return errors
+
+
+def compute_errors(args, data_handler):
+    errors = {}
+    for num_samples in xrange(args.range[0], args.range[1], args.range[2]):
+        errors.update({num_samples: evaluate_sample_number(args, data_handler, num_samples)})
+    return errors
 
 
 def evaluate_sample_number(args, data_handler, num_samples):
@@ -85,35 +119,54 @@ def evaluate_model(args, data_handler, biomarker):
 
 def save_errors(args, data_handler, errors):
     evaluation_file = os.path.join(adni.eval_folder, 'eval_synth_model.csv')
-    nums_samples = np.sort(errors.keys()).tolist()
+    experiments = np.sort(errors.keys()).tolist()
 
     writer = csv.writer(open(evaluation_file, 'wb'), delimiter=',')
-    writer.writerow(['biomarker', 'uniform'] + nums_samples)
+    writer.writerow(['biomarker', 'uniform'] + experiments)
     for biomarker in data_handler.get_biomarker_set():
         for uniform in [True, False]:
             error_curve = []
-            for num_samples in nums_samples:
-                error_curve.append(errors[num_samples][biomarker][uniform])
+            for experiment in experiments:
+                error_curve.append(errors[experiment][biomarker][uniform])
             writer.writerow([biomarker, uniform] + error_curve)
 
 
 def plot_errors(args, data_handler, errors):
     plt.figure()
-    linestyle = {True: '-', False: '--'}
+    linestyle = {True: '--', False: '-'}
     color = {'synth1': 'b', 'synth2': 'g', 'synth3': 'r', 'synth4': 'k'}
 
-    nums_samples = np.sort(errors.keys())
+    experiments = np.sort(errors.keys())
     for biomarker in data_handler.get_biomarker_set():
         for uniform in [True, False]:
             error_curve = []
-            for num_samples in nums_samples:
-                error_curve.append(errors[num_samples][biomarker][uniform])
-            plt.plot(nums_samples, error_curve,
+            for experiment in experiments:
+                error_curve.append(errors[experiment][biomarker][uniform])
+            plt.plot(experiments, error_curve,
                      linestyle=linestyle[uniform], color=color[biomarker],
-                     label='{0} {1}'.format(biomarker, 'unif.' if uniform else ''))
+                     label='{0}{1}'.format(biomarker, ' (unif)' if uniform else ''))
 
     plt.legend()
     plt.show()
+
+
+def analyse_errors(args, data_handler, errors):
+    experiments = np.sort(errors.keys())
+
+    # Compute mean error uniform vs. triangular
+    mean_difference = 0.0
+    for biomarker in data_handler.get_biomarker_set():
+        error_curve_uniform = []
+        error_curve_triangu = []
+        for experiment in experiments:
+            error_curve_uniform.append(errors[experiment][biomarker][True])
+            error_curve_triangu.append(errors[experiment][biomarker][False])
+
+        mean_difference += np.mean(np.array(error_curve_triangu) -
+                                   np.array(error_curve_uniform))
+    mean_difference /= len(data_handler.get_biomarker_set())
+
+    print log.RESULT, 'Mean difference uniform vs. triangular: {0}'.format(mean_difference)
 
 
 if __name__ == '__main__':
