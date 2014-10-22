f0b0d9f0ad43e0bc1a9d1794134f1697e656d2b2
aschmiri <a.schmidtrichberg@gmail.com>
2014-10-22 17:48:42 +0100

    Added simple classification based on the predicted disease progress

--- src/fitting/vgam_estimate_progress.py
+++ src/fitting/vgam_estimate_progress.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python2.7
+import os.path
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from common import log as log
+from common import adni_tools as adni
 from vgam.datahandler import DataHandler
 from vgam.modelfitter import ModelFitter
 import fitting.vgam_evaluation as ve
@@ -18,6 +20,7 @@ def main():
     parser.add_argument('--no_plot', action='store_true', help='do not plot the results')
     parser.add_argument('--plot_steps', type=int, default=15, help='number of steps for the DPI scale')
     parser.add_argument('--plot_file', type=str, default=None, help='filename of the output file')
+    parser.add_argument('--latex_file', type=str, default=None, help='add output to a LaTeX file')
     args = parser.parse_args()
 
     _, diagnoses, dpis, _, mean_min, mean_max = ve.get_progress_estimates(args, args.visits,
@@ -91,11 +94,72 @@ def plot_dpi_estimates(args, dpis, diagnoses, mean_min, mean_max):
 
 def analyse_dpi_estimates(args, dpis, diagnoses):
     # Calculate accuracy
-    misclassified = 0
-    for dpi, diagnosis in zip(dpis, diagnoses):
-        if dpi > 0 and diagnosis == 0.5 or dpi <= 0 and diagnosis == 1.0:
-            misclassified += 1
-    print log.RESULT, 'Accuracy: {0}'.format(1.0 - float(misclassified) / float(len(dpis)))
+    dpis = np.array(dpis)
+    diagnoses = np.array(diagnoses)
+
+    dpis_cn = dpis[np.where(diagnoses == 0.0)]
+    dpis_ad = dpis[np.where(diagnoses == 1.0)]
+    dpis_emci = dpis[np.where(diagnoses == 0.25)]
+    dpis_lmci = dpis[np.where(diagnoses == 0.75)]
+    dpis_mci = np.concatenate((dpis_emci, dpis_lmci))
+
+    # AD - CL
+    acc_cn_ad, t_cn_ad = run_classification(dpis_cn, dpis_ad)
+    print log.RESULT, 'Leave-one-out accuracy CN vs. AD {0} (threshold {1})'.format(acc_cn_ad, t_cn_ad)
+    acc_cn_mci, t_cn_mci = run_classification(dpis_cn, dpis_mci)
+    print log.RESULT, 'Leave-one-out accuracy CN vs. MCI {0} (threshold {1})'.format(acc_cn_mci, t_cn_mci)
+    acc_mci_ad, t_mci_ad = run_classification(dpis_mci, dpis_ad)
+    print log.RESULT, 'Leave-one-out accuracy MCI vs. AD {0} (threshold {1})'.format(acc_mci_ad, t_mci_ad)
+    acc_emci_lmci, t_emci_lmci = run_classification(dpis_emci, dpis_lmci)
+    print log.RESULT, 'Leave-one-out accuracy EMCI vs. LMCI {0} (threshold {1})'.format(acc_emci_lmci, t_emci_lmci)
+
+    if args.latex_file is not None:
+        filename = os.path.join(adni.eval_folder, args.latex_file)
+        with open(filename, 'a') as latex_file:
+            latex_file.write('{0} & {1} & {2:.2f} & {3:.1f} & {4:.2f} & {5:.1f} & {6:.2f} & {7:.1f} & {8:.2f} & {9:.1f}\\\\\n'.format(
+                             args.method,
+                             len(args.visits),
+                             acc_cn_ad, t_cn_ad,
+                             acc_cn_mci, t_cn_mci,
+                             acc_mci_ad, t_mci_ad,
+                             acc_emci_lmci, t_emci_lmci)
+)
+
+def run_classification(dpis1, dpis2):
+    # Assemble data
+    dpis = np.concatenate((dpis1, dpis2))
+    labels = np.concatenate((np.zeros(len(dpis1)), np.ones(len(dpis2))))
+
+    # Sort data
+    indices = np.argsort(dpis)
+    dpis = dpis[indices]
+    labels = labels[indices]
+
+    num_correct = 0
+    thresholds = []
+    for test_i, test_label in enumerate(labels):
+        # Get training data
+        train_dpis = np.delete(dpis, test_i)
+        train_labels = np.delete(labels, test_i)
+
+        # Train optimal threshold
+        accuracies = []
+        for thresh in range(len(train_labels)):
+            accuracies.append(thresh - np.sum(train_labels[:thresh]) + np.sum(train_labels[thresh:]))
+        max_index = np.argmax(accuracies)
+
+        # Get and store threshold
+        threshold = 0.5 * (train_dpis[max_index - 1] + train_dpis[max_index])
+        thresholds.append(threshold)
+
+        # Test test sample
+        if test_label == (0 if dpis[test_i] < threshold else 1):
+            num_correct += 1
+
+    mean_accuracy = float(num_correct) / len(labels)
+    mean_tresh = np.mean(thresholds)
+
+    return mean_accuracy, mean_tresh
 
 
 if __name__ == '__main__':
--- src/vgam_test_all_models.sh
+++ src/vgam_test_all_models.sh
@@ -1,41 +1,41 @@
 #! /bin/bash
-#method=$1
+method=$1
 
 #for method in cog mbl long img all
 #do
-#  for visits in  "bl" "m12" "m24" "bl m12" "m12 m24" "bl m12 m24"
-#  do
-#    fitting/vgam_estimate_progress.py ${visits} -m ${method} --no_plot
-#  done
+  for visits in  "bl" "m12" "m24" "bl m12" "m12 m24" "bl m12 m24"
+  do
+    fitting/vgam_estimate_progress.py ${visits} -m ${method} --no_plot
+  done
 #done
 
 for biomarker in "MMSE" "FAQ" "CDRSB" "Right Hippocampus"
 do
-  for method in cog mbl long img all
-  do
+#  for method in cons #cog mbl long img all
+#  do
     for visits in  "bl" "m12" "m24" "bl m12" "m12 m24" "bl m12 m24"
     do
       fitting/vgam_predict_biomarker_value.py ${visits} -m ${method} -p "${biomarker}" --consistent_data --exclude_cn --latex_file results.tex --no_plot
     done
-  done
+#  done
 done
 
-#for method in cog mbl all img long
+#for method in cons #cog mbl all img long
 #do
-#  for visits in "bl m12" "bl m12 m24" "m12 m24"
-#  do
-#    fitting/vgam_estimate_progress.py ${visits} -m ${method} --estimate_dpr --no_plot
-#  done
+  for visits in "bl m12" "bl m12 m24" "m12 m24"
+  do
+    fitting/vgam_estimate_progress.py ${visits} -m ${method} --estimate_dpr --no_plot
+  done
 #done
 
 for biomarker in "MMSE" "FAQ" "CDRSB" "Right Hippocampus"
 do
-  for method in cog mbl all img long
-  do
+#  for method in cons #cog mbl all img long
+#  do
     for visits in "bl m12" "bl m12 m24" "m12 m24"
     do
       fitting/vgam_predict_biomarker_value.py ${visits} -m ${method} -p "${biomarker}" --estimate_dpr --consistent_data --exclude_cn --latex_file results.tex --no_plot
     done
-  done
+#  done
 done
 
